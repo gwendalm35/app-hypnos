3cd7444470ec61d7c8a42e8a55c63546f742ff1e
gwendalm35 <98034345+gwendalm35@users.noreply.github.com>
2022-04-19 01:13:14 +0200

    add heroku procfile

--- src/Entity/HotelRooms.php
+++ src/Entity/HotelRooms.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use JetBrains\PhpStorm\Pure;
 use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\VichUploaderBundle;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: HotelRoomsRepository::class)]
--- src/Kernel.php
+++ src/Kernel.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Vich\UploaderBundle\VichUploaderBundle;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 class Kernel extends BaseKernel
 {
