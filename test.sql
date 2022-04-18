87fb0cf862b0bd2c05604e6eea5bd9fe54c4b753
gwendalm35 <98034345+gwendalm35@users.noreply.github.com>
2022-04-18 22:47:01 +0200

    My commit

--- test.sql
+++ test.sql
@@ -7,11 +7,6 @@
 -- Version du serveur : 10.4.24-MariaDB
 -- Version de PHP : 8.1.4
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -20,8 +15,7 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `test`
 --
-CREATE DATABASE IF NOT EXISTS `test` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `test`;
+
 
 -- --------------------------------------------------------
 
